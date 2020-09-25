cd08d079ddbcf77f3e9e4d429b906f3b6bc28ae2
Mihir_Prajapati <mihir1737@gmail.com>
2020-09-25 23:14:00 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css"
+ import Home from './components/Home.component'
+ import Register from './components/Register.component'
+ import Login from './components/Login.component'
+ import Navibar from './components/Navibar.component'
+ import Profile from './components/profile.component'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navibar/>
+        <Router>
+        <Route path="/" exact component={Home}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login}/>
+        <Route path='/profile' component={Profile}/>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
